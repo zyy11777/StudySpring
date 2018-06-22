1efddced52aee472063a7e4305f39fe087db59c7
zhangyy <zhangyy@esensoft.com>
2018-06-22 16:32:03 +0800

    创建web工程

---
+++ src/day01/bean/User.java
@@ -0,0 +1,19 @@
+package day01.bean;
+
+public class User {
+	private String name;
+	private Integer age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+}
